f8c09126d3fa7a14d6f9b69473e5b09c42217148
K-Lemmys Repos <31289154+31415pi@users.noreply.github.com>
2022-12-03 23:17:24 -0800

    Create D1.py

---
+++ 2015/D1.py
@@ -0,0 +1,21 @@
+//Advent of Code 2015 Day 1
+//https://adventofcode.com/2015/day/1
+//Part 1.
+//Part 2: Uncomment 6,9,14-17.
+//line 5 is for the iter.
+from functools import partial
+total = 0
+//sum = 0
+with open("data.txt", 'r') as f:
+    for floor in iter(partial(f.read, 1), ''):
+//        sum +=1
+        if (floor == "("):
+            total +=1
+        else:
+            total -=1
+//            if (total == -1):
+//                print("First went to the basermenter:")
+//                print(sum)
+//                exit()
+print("total:")
+print(total)
